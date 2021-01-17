c0be8f6172037a20251fc0727c2330480ef82d2f
s390830 <lukas.moench@stud-mail.uni-wuerzburg.de>
2021-01-17 17:51:41 +0100

    added gitignore, discordjs dependency and basic bot

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const fs = require('fs')
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.channel.send('pong');
+    }
+});
+
+// read token from discord_token file
+const data = fs.readFileSync('./discord_token', 'utf8')
+client.login(data);
