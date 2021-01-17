ad21d998f3524c98d68f752e6c7571eca4f95e2d
s390830 <lukas.moench@stud-mail.uni-wuerzburg.de>
2021-01-17 19:13:57 +0100

    implemented json parsing

--- src/data/FileReader.js
+++ src/data/FileReader.js
@@ -1,7 +1,42 @@
+import Task from "../model/Task.js";
+import Room from "../model/Room.js";
+import roomManager from "../model/RoomManager.js";
+import { taskType } from "../model/Task.js";
 
-
+import fs from "fs";
 
 export default class FileReader {
+  readFile(mysteryKey) {
+    const textData = fs.readFileSync(
+      `./data/mysteries/mystery-${mysteryKey}.json`
+    );
+    const mystery = JSON.parse(textData);
+    Object.entries(mystery.rooms).forEach(([roomName, roomData]) => {
+      const room = new Room(
+        roomName,
+        roomData.textChannels,
+        roomData.speechChannels,
+        roomData.accessCondition
+      );
 
-    readFile(mysteryKey)
+      roomManager.addRoom(room);
+
+      roomData.tasks.forEach((taskName) => {
+        const taskData = JSON.parse(
+          fs.readFileSync(
+            `./data/mysteries/${mysteryKey}/tasks/${taskName}.json`
+          )
+        );
+
+        let task = new Task(
+          taskName,
+          taskType[taskData.type],
+          taskData.solution,
+          mystery.tasks[taskName].followUpRooms
+        );
+
+        roomManager.addTask(task, roomName);
+      });
+    });
+  }
 }
--- src/model/RoomManager.js
+++ src/model/RoomManager.js
@@ -1,17 +1,19 @@
-
 class RoomManager {
+  constructor() {
+    this.rooms = {};
+    this.tasks = {};
+  }
 
-    constructor(){
-        this.rooms = {};
-        this.tasks = {}
-    }
+  addRoom(room) {
+    this.rooms[room.roomName] = room;
+  }
 
-
-    addRoom(room){
-        this.rooms[room.roomName] = room;
-    }
-
-    addTask(task, roomName){
-        this.task.
-    }
+  addTask(task, roomName) {
+    this.tasks[task.name] = task;
+    this.rooms[roomName].addTask(task);
+  }
 }
+
+const roomManager = new RoomManager();
+
+export default roomManager;
